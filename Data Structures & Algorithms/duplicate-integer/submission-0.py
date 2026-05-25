df068acae5a985a738d41bd07e15f9ca91ae18a4
Shanmuk Chadaram <chshanmuk416@gmail.com>
2026-05-25 19:08:19 +0530

    Optimize duplicate check using a set
    Changing from list to set
    
    as set have the hash mapping built in which will provide O(1) for searching the index

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,8 +1,8 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        existing_nums = []
+        existing_nums = set()
         for i in nums:
             if i in existing_nums:
                 return True
-            existing_nums.append(i)
+            existing_nums.add(i)
         return False
